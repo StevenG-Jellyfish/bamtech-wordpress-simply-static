da909bdd9352b388d16103c5e93c875bcd07d489
Steven Gallagher <steven.gallagher@jellyfish.net>
2018-03-09 11:35:47 -0500

    added wpml plugin and work onpotlight

--- wordpress/wp-content/plugins/sitepress-multilingual-cms/classes/language-switcher/class-wpml-get-ls-languages-status.php
+++ wordpress/wp-content/plugins/sitepress-multilingual-cms/classes/language-switcher/class-wpml-get-ls-languages-status.php
@@ -1,7 +1,6 @@
 <?php
 
-class WPML_Get_LS_Languages_Status {
-	private static $the_instance;
+class WPML_Get_LS_Languages_Status extends WPML_Singleton {
 
 	private $in_get_ls_languages = false;
 
@@ -17,20 +16,4 @@ class WPML_Get_LS_Languages_Status {
 		$this->in_get_ls_languages = false;
 	}
 
-	/**
-	 * @return WPML_Get_LS_Languages_Status
-	 */
-	public static function get_instance() {
-		if ( ! self::$the_instance ) {
-			self::$the_instance = new WPML_Get_LS_Languages_Status();
-		}
-		return self::$the_instance;
-	}
-
-	/**
-	 * @param WPML_Get_LS_Languages_Status $instance
-	 */
-	public static function set_instance( $instance ) {
-		self::$the_instance = $instance;
-	}
 }
