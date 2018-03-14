885ac10b9ad84c17c6aa16951c8fffbf9625f532
Steven Gallagher <steven.gallagher@jellyfish.net>
2018-03-14 15:39:48 -0400

    unscrewed bootstrap and jquery

--- src/_themes/espnplus/js/espnplus-bottom.js
+++ src/_themes/espnplus/js/espnplus-bottom.js
@@ -1,3 +1,8 @@
 jQuery(function($) {
     console.log('espnplus bottom.js loaded');
+
+    // execute the following functions when the DOM returns the 'DOMContentLoaded' status (render tree built)
+    $(document).ready(function() {
+        $('.collapse').collapse();     
+    }); // doc.ready
 });
