f1076eaabdf231cceb906a70358ccf6b03b61e1a
Shajahan Miah <shajahan.miah@jellyfish.net>
2018-03-16 12:55:05 -0400

    adding footer links

---
+++ wordpress/wp-content/themes/espnplus/template-parts/components/footer-links.php
@@ -0,0 +1,69 @@
+<?php
+/**
+* Footer component
+*
+* @package lincolntech
+*/
+
+$page_id = get_query_var('page_override_id');
+
+if (empty($page_id)) {
+    $page_id = get_the_ID();
+}
+ 
+/* 
+*  Assign repeater field to $component variable
+*/ 
+$component = get_field('component_footer_link', $page_id);
+
+/* 
+*  Verify if repeater value is empty
+*  Then store the repeater component sections ids in a new array
+*/ 
+if ($component !== false) {
+    
+    //print_r($component);
+    $section_ids = array();
+    
+    foreach($component as $value){
+        $section_ids[] = $value['component_footer_link_item'];
+    }
+    //print_r($section_ids);
+
+    
+    /* 
+     *  Loop $section_ids array and pull each values in respectives areas
+     *  @ footer_link_text
+     *  @ footer_link_URL
+     */ 
+    ?>
+  
+  <section class="footer-links">
+    <div class="link-container">
+        <div class="espn-lang">
+            <div class="logo">
+                <img src="../svgs/E+_logo.svg" alt="ESPN plus logo">
+            </div>
+            <div class="select-wrap">
+                <select name="lang-select">
+                    <option value="en">English</option>
+                    <option value="es">Spanish</option>
+                </select>
+            </div>
+        </div>
+        <div class="espn-links">
+        <?php foreach($section_ids as $section_id) {
+              $link = get_field('footer_link_URL', $section_id);
+        ?>
+            <div class="link">
+                <a href="<?php echo $link;?>"><?php echo get_field('footer_link_text', $section_id);?></a>
+            </div>
+        <?php }?>
+        </div>
+    </div> 
+    <!-- // container -->
+</section>
+<!-- // section --> 
+
+    <?php 
+}
