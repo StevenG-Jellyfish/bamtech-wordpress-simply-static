3be38508891b3da1cc0428950a908aba073fc024
Shajahan Miah <shajahan.miah@jellyfish.net>
2018-03-20 10:02:24 -0400

    adding updated league accordion html to php

--- wordpress/wp-content/themes/espnplus/template-parts/components/league-accordion.php
+++ wordpress/wp-content/themes/espnplus/template-parts/components/league-accordion.php
@@ -31,7 +31,59 @@ if ($component !== false) {
     }
     //print_r($section_ids);
 
-    ?>
+
+/* 
+    *  Loop $section_ids array and pull each values in respectives areas
+    *  @ league_accordion_text
+    *  @ league_accordion_body
+    */  
+$pos_class = array('One','Two','Three','Four','Five','Six','Seven','Eight');
+$acc_count=0;
+
+function card($acc_count, $section_id) {        
+    $text = get_field('league_accordion_text', $section_id);  
+    $body = get_field('league_accordion_body', $section_id);
+    $output = <<< EndHTML
+            <!-- Accordion card -->
+            <div class="card">
+        
+                <!-- Card header -->
+                <div class="card-header" role="tab" id="heading$acc_count">
+                    <a data-toggle="collapse" data-parent="#league-toc-accordion" href="#collapse$acc_count" aria-expanded="false" aria-controls="collapse$acc_count">
+                        <h5 class="mb-0">
+                            $text <i class="fa fa-chevron-down"></i></i>
+                        </h5>
+                    </a>
+                </div>
+                <!-- // Card header -->
+                <!-- Card body -->
+                <div id="collapse$acc_count" class="collapse" role="tabpanel" aria-labelledby="heading$acc_count" data-parent="#league-toc-accordion">
+                    <div class="card-body">
+                        $body
+                    </div>
+                </div>
+                <!-- // Card body -->
+            </div>
+            <!-- // Accordion card -->
+EndHTML;
+return $output;
+} //card()
+
+
+$leftCol='';
+$rightCol='';
+foreach($section_ids as $section_id) {
+    if($acc_count%2 == 0){
+        $leftCol .= card($acc_count, $section_id);  
+    }
+    else {
+        $rightCol .= card($acc_count, $section_id);
+        
+    }
+    $acc_count++;
+} //foreach
+
+?>
 
 
     <!--  Cut just this for the component -->
@@ -41,50 +93,18 @@ if ($component !== false) {
             <!--*** replace this script with php -->
             <div class="copyright">&copy;<script>var d = new Date(); document.write( d.getFullYear() ); </script> ESPN </div>
 
-                <!--Accordion wrapper-->
-                <div class="accordion" id="league-toc-accordion" role="tablist" aria-multiselectable="true">
-
-
-                    <?php
-                    /* 
-                     *  Loop $section_ids array and pull each values in respectives areas
-                     *  @ league_accordion_text
-                     *  @ league_accordion_body
-                     */  
-                    $pos_class = array('One','Two','Three','Four','Five','Six','Seven','Eight');
-                    $acc_count=0;
-                    foreach($section_ids as $section_id) {
-
-                    ?>
-                        <!-- Accordion card -->
-                        <div class="card">
-                    
-                            <!-- Card header -->
-                            <div class="card-header" role="tab" id="heading<?php echo $pos_class[$acc_count]; ?>">
-                                <a data-toggle="collapse" data-parent="#league-toc-accordion" href="#collapse<?php echo $pos_class[$acc_count]; ?>" aria-expanded="false" aria-controls="collapse<?php echo $pos_class[$acc_count]; ?>">
-                                    <h5 class="mb-0">
-                                        <?php echo get_field('league_accordion_text', $section_id);?> <i class="fa fa-chevron-down"></i></i>
-                                    </h5>
-                                </a>
-                            </div>
-                            <!-- // Card header -->
-                            <!-- Card body -->
-                            <div id="collapse<?php echo $pos_class[$acc_count]; ?>" class="collapse" role="tabpanel" aria-labelledby="heading<?php echo $pos_class[$acc_count]; ?>" data-parent="#league-toc-accordion">
-                                <div class="card-body">
-                                    <?php echo get_field('league_accordion_body', $section_id);?>
-                                </div>
-                            </div>
-                            <!-- // Card body -->
-                        </div>
-                        <!-- // Accordion card -->
-                
-                    <?php $acc_count++;} ?>
-
-                    
-        
+            <div class="accordion-container">
+                <div class="accordion" id="league-toc-accordion1" role="tablist" aria-multiselectable="true">
+                    <?php echo $leftCol;?>
                 </div>
-                <!--// Accordion wrapper-->
+                <div class="accordion" id="league-toc-accordion2" role="tablist" aria-multiselectable="true">
+                    <?php echo $rightCol;?>
+                </div>    
+
+            </div>
+            <!--// accordion wrapper-->
         </div>    
+         <!--// container -->
     </section>
     <!--  //End Cut -->
 
