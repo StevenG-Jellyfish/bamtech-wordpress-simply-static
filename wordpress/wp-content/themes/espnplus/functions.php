48c76925cd3b42707db4b4de64000f7be69b6885
Eric Thomas <eric.thomas@jellyfish.net>
2018-03-28 11:12:58 -0400

    header with just logo

--- wordpress/wp-content/themes/espnplus/functions.php
+++ wordpress/wp-content/themes/espnplus/functions.php
@@ -128,15 +128,15 @@ function espnplus_widgets_init() {
 add_action( 'widgets_init', 'espnplus_widgets_init' );
 /* --- */
 //Making jQuery to load from Google Library
-function replace_jquery() {
-	if (!is_admin()) {
-		// comment out the next two lines to load the local copy of jQuery
-		wp_deregister_script('jquery');
-		wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', true, '3.1.1');
-		wp_enqueue_script('jquery');
-	}
-}
-add_action('init', 'replace_jquery');
+// function replace_jquery() {
+// 	if (!is_admin()) {
+// 		// comment out the next two lines to load the local copy of jQuery
+// 		wp_deregister_script('jquery');
+// 		wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', true, '3.1.1');
+// 		wp_enqueue_script('jquery');
+// 	}
+// }
+// add_action('init', 'replace_jquery');
 /* ------------------- */
 /**
  * 
@@ -145,7 +145,7 @@ add_action('init', 'replace_jquery');
 function espnplus_scripts() {
 	wp_enqueue_style( 'espnplus-style', get_stylesheet_uri(),array(),'20151215', false );
 	wp_enqueue_script( 'espnplus-bottom', get_template_directory_uri() . '/js/espnplus-bottom.min.js', array(), '20151215', true );
-
+	wp_enqueue_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', true, '3.1.1');
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
