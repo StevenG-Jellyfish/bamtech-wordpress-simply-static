ccbf675a219cfad9f8116a2a254af3f1dc36f9bd
Eric Thomas <eric.thomas@jellyfish.net>
2018-04-05 11:14:47 -0400

    404 page

--- wordpress/wp-content/themes/espnplus/404.php
+++ wordpress/wp-content/themes/espnplus/404.php
@@ -6,50 +6,25 @@
  *
  * @package espnplus
  */
-
 get_header();
 ?>
-
-	<div id="primary" class="content-area">
+	<div id="page" class="content-area 404-page site">
 		<main id="main" class="site-main">
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'espnplus' ); ?></h1>
+					<h1 class="page-title">
+						<?php esc_html_e( 'Oops! That page can&rsquo;t be found. 404', 'espnplus' ); ?>
+					</h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'espnplus' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'espnplus' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$espnplus_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'espnplus' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$espnplus_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+					<img src="<?php bloginfo('template_url'); ?>/imgs/404.png" alt="404">
+					<div class="error-msg">
+						<div class="">The page you are looking for can't be found. Please <a href="<?php echo get_site_url(); ?>">click here</a> to return to the homepage.</div>
+					</div>
+					<div class="error404-divider"><div class="divider"></div></div>
+					<img src="<?php bloginfo('template_url'); ?>/imgs/404-img.png" alt="404">
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
