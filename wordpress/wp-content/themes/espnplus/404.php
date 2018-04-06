e45640ae95785d0ce73376c054a016b81de638d2
mauricio-jellyfish <mauricio.masias@jellyfish.co.uk>
2018-04-06 14:01:23 +0100

    404 page comments removed

--- wordpress/wp-content/themes/espnplus/404.php
+++ wordpress/wp-content/themes/espnplus/404.php
@@ -12,11 +12,10 @@ get_header();
         <div class="headerbox">
                 <div class="site-header center">
                     <div class="site-branding">
-					<?php the_custom_logo() ?>
-                        <!-- <img src="the_custom_logo()" alt="ESPN+"> -->
+					<?php the_custom_logo() ?> 
                     </div> 
                 </div>
-                <!-- </div> -->
+               
         </div>
     </header>
 	<div id="page" class="content-area 404-page site">
@@ -27,7 +26,7 @@ get_header();
 					<h1 class="page-title">
 						<?php esc_html_e( 'Oops! That page can&rsquo;t be found. 404', 'espnplus' ); ?>
 					</h1>
-				</header><!-- .page-header -->
+				</header>
 
 				<div class="page-content">
 					<img src="<?php bloginfo('template_url'); ?>/imgs/404.png" alt="404">
@@ -39,11 +38,11 @@ get_header();
 						<source media="(max-width: 480px)" srcset="<?php bloginfo('template_url'); ?>/imgs/hero-1280-generic.png">
 						<img class="main-404-img" src="<?php bloginfo('template_url'); ?>/imgs/hero-1280-generic-2x.png" alt="404">
 					</picture>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+				</div>
+			</section>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</main>
+	</div>
 
 <?php
 get_footer();
