2094b8b27ba720ea2379c8ab385ccecf34cc2449
mauricio-jellyfish <mauricio.masias@jellyfish.co.uk>
2018-04-10 12:50:15 +0100

    analytics league and sport variables

--- wordpress/wp-content/themes/espnplus/js/espnplus-non-critical.js
+++ wordpress/wp-content/themes/espnplus/js/espnplus-non-critical.js
@@ -1,5 +1,5 @@
 /*find current language*/
-var language = (LangCode=='es')? "es":"en-us";
+var ALanguage = (LangCode=='es')? "es":"en-us";
 
  /* Configuration Variables */
 s_omni.pageName="espnplus:marketing:paywall";
@@ -15,11 +15,11 @@ s_omni.contextData["externalmkt"] = s_omni.Util.getQueryParam("ex_cid");
 s_omni.contextData["paywallshown"] = "yes";
 s_omni.contextData["site"] = "espnplus";
 s_omni.contextData["registrationtype"] = "unknown";
-s_omni.contextData["lang"] = "en_us";
+s_omni.contextData["lang"] = ALanguage;
 s_omni.contextData["visitortype"] = s_omni.getNewRepeat(30, "s_getNewRepeat");
-s_omni.contextData["sport"] = "no sport";
-s_omni.contextData["league"] = "no league";
-s_omni.contextData["edition"] = language;
+s_omni.contextData["sport"] = ASport;
+s_omni.contextData["league"] = ALeague;
+s_omni.contextData["edition"] = ALanguage;
 s_omni.contextData["paywallvisitcount"] = s_omni.getVisitNum();
 s_omni.contextData["lastvisit"] = s_omni.getDaysSinceLastVisit();
 
@@ -28,16 +28,15 @@ var s_code=s_omni.t();if(s_code)document.write(s_code);
  window.jQuery && $(function() {
         /* Clicks on header CTA */
         $("#header_cta").on("click", function(t) {
-            t.preventDefault();
-            var language = (LangCode=='es')? "es":"en-us";
-            var catDays = ctaDays($(this).html(),language);
             
-           
+            t.preventDefault();
+            var catDays = ctaDays($(this).html(),ALanguage);
+             
             try {
                 var r = s_gi(s_account);
                 r.linkTrackVars = "products,contextData.edition,contextData.site,contextData.linkid,contextData.purchasemethod,contextData.buylocation";
                 r.linkTrackEvents = "";
-                r.contextData["edition"] = language;
+                r.contextData["edition"] = ALanguage;
                 r.contextData["site"] = "espnplus";
                 r.contextData["linkid"] = "buy:espn+monthly:"+catDays+"ft";
                 r.contextData["purchasemethod"] = "bamtech";
@@ -52,16 +51,15 @@ var s_code=s_omni.t();if(s_code)document.write(s_code);
         });
         /* Clicks on spotlight CTA */
         $("#spotlight_cta").on("click", function(t) {
-            t.preventDefault();
-            var language = (LangCode=='es')? "es":"en-us";
-            var catDays = ctaDays($(this).html(),language);
             
+            t.preventDefault();
+            var catDays = ctaDays($(this).html(),ALanguage);
 
             try {
                 var r = s_gi(s_account);
                 r.linkTrackVars = "products,contextData.edition,contextData.site,contextData.linkid,contextData.purchasemethod,contextData.buylocation";
                 r.linkTrackEvents = "";
-                r.contextData["edition"] = language;
+                r.contextData["edition"] = ALanguage;
                 r.contextData["site"] = "espnplus";
                 r.contextData["linkid"] = "buy:espn+monthly:"+catDays+"ft";
                 r.contextData["purchasemethod"] = "bamtech";
