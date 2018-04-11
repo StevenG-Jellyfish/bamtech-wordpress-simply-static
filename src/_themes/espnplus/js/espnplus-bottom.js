69df8bcced4d0620f34e9099416417eea9131893
Steven Gallagher <steven.gallagher@jellyfish.net>
2018-04-11 18:31:50 -0400

    added explicit call to bind collapse

--- src/_themes/espnplus/js/espnplus-bottom.js
+++ src/_themes/espnplus/js/espnplus-bottom.js
@@ -1,6 +1,7 @@
 jQuery(function($) {
     //console.log('espnplus bottom.js loaded');
-
+   $('.collapse').collapse(); 
+   
    /*find current language*/
     var ALanguage = (LangCode=='es')? "es":"en-us";
     var Ex_cid = s_omni.Util.getQueryParam("ex_cid");
