c40d50e05c6c1e4c3201fb111e6581fa11bf17e2
Steven Gallagher <steven.gallagher@jellyfish.net>
2018-04-24 10:35:17 -0400

    BAMT-243: updated Adobe calls in bottom.js; pulled s_omni out of onReady function

--- src/_themes/espnplus/js/espnplus-bottom.js
+++ src/_themes/espnplus/js/espnplus-bottom.js
@@ -1,34 +1,34 @@
+/*find current language*/
+var ALanguage = (LangCode=='es')? "es":"en-us";
+var Ex_cid = s_omni.Util.getQueryParam("ex_cid");
+
+/* Configuration Variables */
+s_omni.pageName="espnplus:marketing:paywall";
+s_omni.server=window.location.hostname;
+
+/* Conversion Variables */
+s_omni.products="D2C;8400199910209919951899000";
+
+/* Context Variables */
+s_omni.contextData["section"] = "espnplus";
+s_omni.contextData["contenttype"] = "paywall";
+s_omni.contextData["externalmkt"] = Ex_cid; 
+s_omni.contextData["paywallshown"] = "yes";
+s_omni.contextData["site"] = "espnplus";
+s_omni.contextData["registrationtype"] = "unknown";
+s_omni.contextData["lang"] = ALanguage;
+s_omni.contextData["visitortype"] = s_omni.getNewRepeat(30, "s_getNewRepeat");
+s_omni.contextData["sport"] = ASport;
+s_omni.contextData["league"] = ALeague;
+s_omni.contextData["edition"] = ALanguage;
+s_omni.contextData["paywallvisitcount"] = s_omni.getVisitNum();
+s_omni.contextData["lastvisit"] = s_omni.getDaysSinceLastVisit("s_last");
+s_omni.contextData["navmethod"] = "external marketing";
+
+var s_code=s_omni.t();if(s_code)document.write(s_code);
+
 jQuery(function($) {
-    //console.log('espnplus bottom.js loaded');
-
-    /*find current language*/
-    var ALanguage = (LangCode=='es')? "es":"en-us";
-    var Ex_cid = s_omni.Util.getQueryParam("ex_cid");
-
-    /* Configuration Variables */
-    s_omni.pageName="espnplus:marketing:paywall";
-    s_omni.server=window.location.hostname;
-
-    /* Conversion Variables */
-    s_omni.products="D2C;8400199910209919951899000";
-
-    /* Context Variables */
-    s_omni.contextData["section"] = "espnplus";
-    s_omni.contextData["contenttype"] = "paywall";
-    s_omni.contextData["externalmkt"] = Ex_cid; 
-    s_omni.contextData["paywallshown"] = "yes";
-    s_omni.contextData["site"] = "espnplus";
-    s_omni.contextData["registrationtype"] = "unknown";
-    s_omni.contextData["lang"] = ALanguage;
-    s_omni.contextData["visitortype"] = s_omni.getNewRepeat(30, "s_getNewRepeat");
-    s_omni.contextData["sport"] = ASport;
-    s_omni.contextData["league"] = ALeague;
-    s_omni.contextData["edition"] = ALanguage;
-    s_omni.contextData["paywallvisitcount"] = s_omni.getVisitNum();
-    s_omni.contextData["lastvisit"] = s_omni.getDaysSinceLastVisit();
-    s_omni.contextData["navmethod"] = "external marketing";
-
-    var s_code=s_omni.t();if(s_code)document.write(s_code);
+    //console.log('espnplus bottom.js loaded');    
 
      /* Clicks on header CTA */
      $("#header_cta").on("click", function(t) {
